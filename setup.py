5cabf1e2eb4ee44f5329b686a77e1f214e10122c
R00tdroid <info@rootdroid.nl>
2024-04-10 13:39:31 +0200

    Add info

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='uepacker',
+    name='Unreal Engine Packer',
     version='1.0',
     packages=find_packages(),
     entry_points={
@@ -11,8 +11,8 @@ setup(
     },
     install_requires=[
     ],
-    author='Your Name',
-    description='Description of your package',
+    author='R00tDroid',
+    description='A commandline tool to automatically package Unreal Engine projects or plugins',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/R00tDroid/UnrealPacker',
