478c7476156678739726f465ea0dcea6ef288a15
R00tdroid <info@rootdroid.nl>
2024-04-11 10:01:29 +0200

    Increase version number to 1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Unreal-Engine-Packer',
-    version='1.1',
+    version='1.2',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
